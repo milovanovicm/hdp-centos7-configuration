43ef4563df8ec15da7722c2d6a3e894bf4784c1e
milovanovicm <milovanovicm@users.noreply.github.com>
2016-06-01 16:28:45 +0200

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # SET PROXY
-echo proxy=proxy.fon.rs:8080 >> /etc/environment
-echo proxy=proxy.fon.rs:8080 >> /etc/yum.conf
+echo http_proxy=proxy.fon.rs:8080 >> /etc/environment
+echo http_proxy=proxy.fon.rs:8080 >> /etc/yum.conf
 export http_proxy=proxy.fon.rs:8080
 
 # Install required packages
@@ -8,6 +9,7 @@ cd ~
 mkdir /home/hadoop
 yum install unzip -y
 yum install wget -y
+yum install nano -y
 ulimit -n 12000
 
 # Passwordless
@@ -23,12 +25,18 @@ yum install ntp ntpdate ntp-doc -y
 systemctl enable ntpd
 systemctl start ntpd
 # EDIT THE HOSTS FILE
+while IFS=' ' read -r line || [[ -n "$line" ]]; do
+    echo "$line" >> /etc/hosts
+done < ips.txt
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
 # 1.4.4.3. Edit the Network Configuration File
 # systemctl disable firewalld
 # service firewalld stop
 
 # Disable SELINUX
 setenforce 0
+sed -n -i '/SELINUX=enforcing/!p' /etc/selinux/config
 echo SELINUX=disabled >> /etc/selinux/config
 
 # Umask
